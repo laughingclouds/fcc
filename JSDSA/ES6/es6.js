a02e04e0043a22fdfc304a97e9a715cce6547e2a
laughingclouds <zenithtaiga@gmail.com>
2021-06-08 01:50:13 +0530

    Added more JS

--- JSDSA/ES6/es6.js
+++ JSDSA/ES6/es6.js
@@ -48,3 +48,22 @@ function freezeObj() {
 
 // Use Arrow Functions to Write Concise Anonymous FunctionsPassed
 let magic = () => new Date();
+
+// Write Arrow Functions with Parameters
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+const doubler = item => item * 2;
+const multiplier = (item, multi) => item * multi;
+
+// Set Default Parameters for Your Functions
+const greeting = (name = "Anonymous") => "Hello " + name;
+const incremet = (number, value = 1) => number + value;
+
+// Use the Rest Parameter with Function Parameters
+function howMany(...args) {
+    return "You have passed " + args.length + " arguments.";
+}
+
+const callBckForSum = (acc, cur) => acc + cur;
+const sum = (...args) => args.reduce(callBckForSum, 0);
